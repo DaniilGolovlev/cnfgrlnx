d86cc4abf79bd471f1937e35c2d37525fa3f298a
DaniilGolovlev <daniil.golovlev@yandex.ru>
2018-10-14 02:47:19 +0300

    16

--- main.py
+++ main.py
@@ -8,18 +8,20 @@
 
 from platform import dist, system
 
+supported_operating_systems = ("debian", "centos")
+
 def select_script():
     """ Выбор скрипта настройки """
 
     os_distr = dist()
 
-    if os_distr == "debian":
+    if os_distr[0] == "debian":
         import OS.cnfgr_debian
-    elif os_distr == "centos":
+    elif os_distr[0] == "centos":
         import OS.cnfgr_centos
-    elif os_distr == "fedora":
+    elif os_distr[0] == "fedora":
         import OS.cnfgr_fedora
-    elif os_distr == "ubuntu":
+    elif os_distr[0] == "ubuntu":
         import OS.cnfgr_ubuntu
 
 
